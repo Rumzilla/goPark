14c5b14e992837c6de1111a39354faa201b13feb
Rumzilla <wow7am@gmail.com>
2023-03-11 18:01:16 +0600

    suspense added to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
@@ -7,8 +7,8 @@ import {BrowserRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <React.StrictMode>
+        <Suspense fallback={<div>Loading...</div>}>
             <App/>
-        </React.StrictMode>
+        </Suspense>
     </BrowserRouter>
 );
