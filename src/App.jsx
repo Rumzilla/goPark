7ca6bdc1aabe2c0061bb05cf79ee525da409e52a
JussVi <osadze_denis_007@mail.ru>
2023-03-11 18:42:15 +0600

    Fix Problems

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import {Route, Switch} from "react-router-dom";
 import MainContent from "./components/MainContent/MainContent";
 import SwiperBlock from "./components/swiper/Swiper";
 import Footer from "./components/footer/footer";
+import Header from './components/header/header';
 
 function App() {
     return (
       <>
+      <Header/>
         <div className="container">
           <Switch>
             <Route exact path='/' component={MainContent}/>
