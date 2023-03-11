eaf9d5f4b7a9c8f84fa73730918e41a23fbdb27f
Askar Kulenbekov <oscarquell96@gmail.com>
2023-03-11 17:05:43 +0600

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import {Route, Switch} from "react-router-dom";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import SwiperBlock from "./components/swiper/Swiper";
 import React from 'react'
 
 
@@ -13,7 +12,6 @@ function App() {
             <Route exact path='/header' component={Header}/>
             <Route exact path='/footer' component={Footer}/>
             </Switch>
-          <SwiperBlock />
         </div>
     );
 }
