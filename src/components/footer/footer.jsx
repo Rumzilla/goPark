e856ba7bcd57a8ad79a0145e0d8d42656f83833a
“Alihan <alihan_akylbekov@mail.ru>
2023-03-13 15:50:23 +0600

    ww

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -16,9 +16,9 @@ const Footer = () => {
               <a className='social-links email-icon' href="/"></a>
             </div>
           <div className="page-navigations">
-              <Link className='page-links' to="/about">{t("about")}</Link>
-              <Link className='page-links' to="/news">{t("news")}</Link>
-              <Link className='page-links' to="/contact">{t("contact")}</Link>
+              <a className='page-links' href="/about">{t("about")}</a>
+              <a className='page-links' href="/news">{t("news")}</a>
+              <a className='page-links' href="/contact">{t("contact")}</a>
             </div>
           <Link to="/" className="head-icon-for-footer"></Link>
           <span className="footer-description">
